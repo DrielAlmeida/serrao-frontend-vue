c69d1415f6818828c718e6f79735352bea17ebaf
Adriel <adriel@aguiabranca.com.br>
2026-05-12 15:13:56 -0300

    ajustando reload

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import LoginView from './views/LoginView.vue'
 import LancamentoView from './views/LancamentoView.vue'
 import ItemCadastro from './views/ItemCadastro.vue'
@@ -44,7 +44,7 @@ const AUTH_KEY = 'serrao-auth'
 const isAuthenticated = () => localStorage.getItem(AUTH_KEY) === 'true'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
